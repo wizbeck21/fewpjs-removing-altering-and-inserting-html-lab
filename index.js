cebb82d1d186f1da0605bb7f5064ba3cec29f3ae
Wesley <wes.beck21@outlook.com>
2021-02-17 10:33:31 -0800

    commit tree 24cde4587aa64b74f6cb532ba2888f5bf2a5a333

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = 'Wizzles is the champion!';
+document.body.appendChild(newHeader);
+
+
